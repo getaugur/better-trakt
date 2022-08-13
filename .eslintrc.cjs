a66ef934bc0d26f54de95d3e9d2fb4b07d8865e4
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-08-13 13:47:32 -0400

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+};
