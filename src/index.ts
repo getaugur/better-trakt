c621cb934de3357d037a5296aa47b061fec59166
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-08-20 17:00:54 -0400

    fix(certifications): export certifications from SDK

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from "./client";
-export * from "./trakt";
-export * from "./users";
-export * from "./media";
-export { Trakt as default } from "./client";
+export * from './client';
+export * from './trakt';
+export * from './users';
+export * from './media';
+export * from './certifications';
+export { Trakt as default } from './client';
