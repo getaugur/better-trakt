e773414089559ee12b05445d34c7433a8fb14c74
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-08-19 22:37:27 -0400

    docs(example): added js example for lib

---
+++ example/index.js
@@ -0,0 +1,21 @@
+// load env vars
+// require('dotenv').config();
+
+// console.log(process.env);
+
+const trakt = require('../dist/index');
+const Trakt = trakt.Trakt;
+
+const clientId = process.env.TRAKT_ID || '';
+
+const client = new Trakt({
+  clientId: clientId,
+});
+
+// hack so we can use async
+(async () => {
+  // console.log(await client.shows.summary('1'));
+
+  console.log(await client.certifications.listMovies());
+  console.log(await client.certifications.listShows());
+})();
