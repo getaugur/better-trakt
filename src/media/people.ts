bf4a8c78660dfc7511a4fee05c5b5363fb5ff10c
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-08-24 16:24:29 -0400

    refactor(media): centralize endpoints

--- src/media/people.ts
+++ src/media/people.ts
@@ -3,14 +3,14 @@ import { MoviePeople, ShowPeople } from '../trakt';
 import { fetch } from '../utils/fetch';
 
 export async function getShowPeople({ client, apiUrl }: ApiConfig, showId: string) {
-  const url = `${apiUrl}/shows/${showId}?extended=full`;
+  const url = `${apiUrl}/${showId}?extended=full`;
   const response = await fetch<ShowPeople>(client, url);
 
   return response.data;
 }
 
 export async function getMoviePeople({ client, apiUrl }: ApiConfig, movieId: string) {
-  const url = `${apiUrl}/movies/${movieId}?extended=full`;
+  const url = `${apiUrl}/${movieId}?extended=full`;
   const response = await fetch<MoviePeople>(client, url);
 
   return response.data;
