d6edf65e3d192d5187a4c9f48aa99e1eeb990c21
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-08-24 16:27:16 -0400

    refactor(users): centralize endpoints

--- src/users/watched.ts
+++ src/users/watched.ts
@@ -3,14 +3,14 @@ import { WatchedMovie, WatchedShow } from '../trakt';
 import { fetch } from '../utils/fetch';
 
 export async function getWatchedShows({ client, apiUrl }: ApiConfig, userId: string, accessToken?: string) {
-  const url = `${apiUrl}/users/${userId}/watched/shows`;
+  const url = `${apiUrl}/${userId}/watched/shows`;
   const response = await fetch<WatchedShow[]>(client, url, accessToken);
 
   return response.data;
 }
 
 export async function getWatchedMovies({ client, apiUrl }: ApiConfig, userId: string, accessToken?: string) {
-  const url = `${apiUrl}/users/${userId}/watched/movies`;
+  const url = `${apiUrl}/${userId}/watched/movies`;
   const response = await fetch<WatchedMovie[]>(client, url, accessToken);
 
   return response.data;
