76bac79c32b4a3faee233c9b7af1ad2e9f477346
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-09-20 18:14:36 -0400

    feat(media)!: add trending & popular
    Also brought all the methods up to standard.

--- src/media/index.ts
+++ src/media/index.ts
@@ -1,6 +1,14 @@
 import { ApiConfig, ApiNamespace } from '../client';
-import { MoviePeople, MovieSummary_Full, ShowPeople, ShowSummary_Full } from '../trakt';
-import { fetch } from '../utils';
+import {
+  MoviePeople,
+  MovieSummary_Full,
+  ShowPeople,
+  ShowSummary_Full,
+  TraktApiContent,
+  TrendingMovie,
+  TrendingShow,
+} from '../trakt';
+import { ApiResponse, fetch, Filters, Pagination } from '../utils';
 import { checkRequiredArg } from '../utils/requiredArg';
 
 /**
@@ -17,26 +25,62 @@ export class Shows implements ApiNamespace {
   }
 
   /**
-   * Get details about a show
+   * Returns a single shows's details.
    * @param showId show id
    * @returns
    */
-  summary({ showId }: { showId: string }) {
+  summary({ showId }: { showId: string }): Promise<ApiResponse<ShowSummary_Full>> {
     checkRequiredArg(showId, 'showId', 'string');
 
     return getMediaSummary_Full<ShowSummary_Full>(this.config, showId);
   }
 
   /**
-   * Get people from a show
+   * Returns all cast and crew for a show.
    * @param showId show id
    * @returns
    */
-  people({ showId }: { showId: string }) {
+  people({ showId }: { showId: string }): Promise<ApiResponse<ShowPeople>> {
     checkRequiredArg(showId, 'showId', 'string');
 
     return getMediaPeople<ShowPeople>(this.config, showId);
   }
+
+  /**
+   * Returns all shows being watched right now.
+   * @param pagination
+   * @returns
+   */
+  trending({
+    pagination,
+    filters,
+  }: {
+    pagination: Pagination;
+    filters?: Filters;
+  }): Promise<ApiResponse<TrendingShow[]>> {
+    checkRequiredArg(pagination, 'pagination', 'object');
+
+    return getTrendingMedia<TrendingShow[]>(this.config, 'shows', { pagination, filters });
+  }
+
+  /**
+   * Returns the most popular shows.
+   * @param pagination
+   * @returns
+   */
+  popular({
+    pagination,
+    filters,
+  }: {
+    pagination: Pagination;
+    filters?: Filters;
+  }): Promise<ApiResponse<TraktApiContent[]>> {
+    checkRequiredArg(pagination, 'pagination', 'object');
+
+    return getPopularMedia(this.config, 'movies', { pagination, filters });
+  }
+
+  recommended({ pagination, filters }: { pagination: Pagination; filters?: Filters });
 }
 
 /**
@@ -52,26 +96,108 @@ export class Movies implements ApiNamespace {
   }
 
   /**
-   * Get details about a movie
+   * Returns a single movie's details.
    * @param movieId movie id
    * @returns
    */
-  summary({ movieId }: { movieId: string }) {
+  summary({ movieId }: { movieId: string }): Promise<ApiResponse<MovieSummary_Full>> {
     checkRequiredArg(movieId, 'movieId', 'string');
 
     return getMediaSummary_Full<MovieSummary_Full>(this.config, movieId);
   }
 
   /**
-   * Get people from a movie
+   * Returns all cast and crew for a movie.
    * @param movieId movie id
    * @returns
    */
-  people({ movieId }: { movieId: string }) {
+  people({ movieId }: { movieId: string }): Promise<ApiResponse<MoviePeople>> {
     checkRequiredArg(movieId, 'movieId', 'string');
 
     return getMediaPeople<MoviePeople>(this.config, movieId);
   }
+
+  /**
+   * Returns all movies being watched right now.
+   * @param pagination
+   * @returns
+   */
+  trending({
+    pagination,
+    filters,
+  }: {
+    pagination: Pagination;
+    filters?: Filters;
+  }): Promise<ApiResponse<TrendingMovie[]>> {
+    checkRequiredArg(pagination, 'pagination', 'object');
+
+    return getTrendingMedia<TrendingMovie[]>(this.config, 'movies', { pagination, filters });
+  }
+
+  /**
+   * Returns the most popular movies.
+   * @param pagination
+   * @returns
+   */
+  popular({
+    pagination,
+    filters,
+  }: {
+    pagination: Pagination;
+    filters?: Filters;
+  }): Promise<ApiResponse<TraktApiContent[]>> {
+    checkRequiredArg(pagination, 'pagination', 'object');
+
+    return getPopularMedia(this.config, 'movies', { pagination, filters });
+  }
+}
+
+/**
+ *
+ * @param ApiConfig
+ * @param type
+ * @returns
+ * @internal
+ */
+export async function getTrendingMedia<T>(
+  { client, apiUrl }: ApiConfig,
+  type: 'movies' | 'shows',
+  {
+    pagination,
+    filters,
+  }: {
+    pagination: Pagination;
+    filters?: Filters;
+  },
+) {
+  const url = `${apiUrl}/${type}/trending`;
+  const response = await fetch<T>(client, url, { pagination, filters });
+
+  return response;
+}
+
+/**
+ *
+ * @param ApiConfig
+ * @param type
+ * @returns
+ * @internal
+ */
+export async function getPopularMedia(
+  { client, apiUrl }: ApiConfig,
+  type: 'movies' | 'shows',
+  {
+    pagination,
+    filters,
+  }: {
+    pagination: Pagination;
+    filters?: Filters;
+  },
+) {
+  const url = `${apiUrl}/${type}/popular`;
+  const response = await fetch<TraktApiContent[]>(client, url, { pagination, filters });
+
+  return response;
 }
 
 /**
--- src/trakt/media/index.ts
+++ src/trakt/media/index.ts
@@ -1,2 +1,3 @@
 export * from './people';
 export * from './summary';
+export * from './trending';
